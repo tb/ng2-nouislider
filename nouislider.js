6beab9740c21bdd15a9c7555037e0cf3055ec7e1
tb <t.bak@selleo.com>
2016-04-24 15:18:24 +0200

    update js

--- nouislider.js
+++ nouislider.js
@@ -1,3 +1,4 @@
+"use strict";
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -95,6 +96,6 @@ var Nouislider = (function () {
         __metadata('design:paramtypes', [core_1.ElementRef])
     ], Nouislider);
     return Nouislider;
-})();
+}());
 exports.Nouislider = Nouislider;
 //# sourceMappingURL=nouislider.js.map
