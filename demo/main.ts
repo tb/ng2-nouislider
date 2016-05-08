1b60b6def5dded27106c6da98c247df50ae17f27
tb <t.bak@selleo.com>
2016-05-08 14:27:48 +0200

    added gh-pages as submodule

--- demo/main.ts
+++
@@ -1,7 +0,0 @@
-import {bootstrap} from '@angular/platform-browser-dynamic';
-import {App} from './app';
-
-document.addEventListener('DOMContentLoaded', function main() {
-  bootstrap(App, [])
-    .catch(err => console.error(err));
-});
--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,8 @@ var path = require('path');
 var webpack = require('webpack');
 
 var ENV = process.env.npm_lifecycle_event;
-var isTest = ENV === 'test';
-var isProd = ENV === 'build:demo';
+var isTest = /test/.test(ENV);
+var isProd = /demo$/.test(ENV);
 
 module.exports = function makeWebpackConfig() {
   var config = {};
