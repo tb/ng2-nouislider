6ea012eabc889ed9d4cb12d02501d9fd467d1d85
tb <t.bak@selleo.com>
2017-01-28 19:29:35 +0100

    Updates

--- Users/tb/projects/node/ng2-nouislider/demo/app.component.d.ts
+++ Users/tb/projects/node/ng2-nouislider/demo/app.component.d.ts
@@ -1,22 +1,28 @@
 import { OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import './app.component.scss';
+import { NouiFormatter } from '../src/nouislider';
+export declare class TimeFormatter implements NouiFormatter {
+    to(value: number): string;
+    from(value: string): number;
+}
 export declare class AppComponent implements OnInit {
     private formBuilder;
     disabled: boolean;
     keyupLabelOn: boolean;
     keydownLabelOn: boolean;
     someValue: number;
-    someFormValue: number;
     someRange: number[];
     someRange2: number[];
     someRange3: number[];
+    simeTime: number;
     someRange2config: any;
     someKeyboard: number[];
     someKeyboardConfig: any;
     someKeyboard2: number[];
     form1: FormGroup;
     form2: FormGroup;
+    someTimeConfig: any;
     constructor(formBuilder: FormBuilder);
     ngOnInit(): void;
     someKeyboard2EventHandler: (e: KeyboardEvent) => void;
