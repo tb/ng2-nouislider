e667b006c24a8191375bd75852542b9a9b243a4b
tb <t.bak@selleo.com>
2017-09-17 11:50:35 +0200

    Publish 1.7.1 - fix export

--- src/nouislider.js
+++ src/nouislider.js
@@ -1,5 +1,4 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 var noUiSlider = require("nouislider");
 var core_1 = require("@angular/core");
 var forms_1 = require("@angular/forms");
