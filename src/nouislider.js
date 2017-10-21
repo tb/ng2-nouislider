de66396b3feeddeaa0c7b72a9e3c108ddf0edcda
tb <t.bak@selleo.com>
2017-10-21 23:21:26 +0200

    Publish 1.7.3 - aot fix

--- src/nouislider.js
+++ src/nouislider.js
@@ -1,4 +1,5 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 var noUiSlider = require("nouislider");
 var core_1 = require("@angular/core");
 var forms_1 = require("@angular/forms");
