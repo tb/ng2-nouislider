aa6925c99fc59c2d6af360e58146e6719faf84b0
Matt Lewis <hello@mattlewis.me>
2023-05-18 20:10:50 +0100

    chore: rescaffold project with angular cli

--- karma.conf.js
+++ karma.conf.js
@@ -1,80 +1,44 @@
-// Karma configuration
-module.exports = function(config) {
+// Karma configuration file, see link for more information
+// https://karma-runner.github.io/1.0/config/configuration-file.html
+
+module.exports = function (config) {
   config.set({
-    // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
-
-    // list of files / patterns to load in the browser
-    files: [
-      { pattern: './karma-shim.js', watched: false },
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma')
     ],
-
-    // list of files to exclude
-    exclude: [],
-
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-      './karma-shim.js': ['webpack', 'sourcemap']
-    },
-
-    webpack: {
-      context: __dirname,
-      devtool: 'inline-source-map',
-      resolve: {
-        modulesDirectories: [
-          'node_modules'
-        ],
-        extensions: ['', '.js', '.ts', '.json', '.scss', '.css', '.html']
+    client: {
+      jasmine: {
+        // you can add configuration options for Jasmine here
+        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
+        // for example, you can disable the random execution with `random: false`
+        // or set a specific seed with `seed: 4321`
       },
-      module: {
-        loaders: [
-          {
-            test: /\.ts$/,
-            loaders: ['ts']
-          }
-        ]
-      },
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-
-    webpackMiddleware: {
-      // webpack-dev-middleware configuration
-      // i. e.
-      stats: 'errors-only'
+    jasmineHtmlReporter: {
+      suppressAll: true // removes the duplicated traces
     },
-
-    webpackServer: {
-      noInfo: true // please don't spam the console when running in karma!
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage/ng2-nouislider'),
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' }
+      ]
     },
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
-
-    // web server port
+    reporters: ['progress', 'kjhtml'],
     port: 9876,
-
-    // enable / disable colors in the output (reporters and logs)
     colors: true,
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
-
-    // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['PhantomJS'],
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
+    browsers: ['Chrome'],
     singleRun: false,
-  })
+    restartOnFileChange: true
+  });
 };
