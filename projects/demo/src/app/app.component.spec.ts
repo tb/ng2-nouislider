186c3d8cb08df081bd0bf1d7d424dad0fba24063
Matt Lewis <hello@mattlewis.me>
2023-05-18 20:17:02 +0100

    refactor: move demo app into projects directory

--- projects/demo/src/app/app.component.spec.ts
+++ projects/demo/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ng2-nouislider'`, () => {
+  it(`should have as title 'demo'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ng2-nouislider');
+    expect(app.title).toEqual('demo');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ng2-nouislider app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('demo app is running!');
   });
 });
